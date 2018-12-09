f52a52ac7e7660e9c9c19545003f38548258d7c1
Ryan Wire <ryanwire@outlook.com>
2018-12-09 11:29:44 +0300

    [Finishes #162530038] Adds connection of redflags model to database

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask import Blueprint
 from app.api.v2.users.views import User, Users, UserSignUp, UserSignIn
 from app.api.v2.incidents.views import Intervention, Interventions, UpdateInterventionStatus, UpdateInterventionLocation, UpdateInterventionComment
+from app.api.v2.incidents.views import Redflag, Redflags, UpdateRedflagStatus, UpdateRedflagLocation, UpdateRedflagComment
 
 VERSION_TWO = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 
@@ -17,3 +18,9 @@ API.add_resource(Intervention, '/interventions/<int:intervention_id>')
 API.add_resource(UpdateInterventionStatus, '/interventions/<int:intervention_id>/status')
 API.add_resource(UpdateInterventionLocation, '/interventions/<int:intervention_id>/location')
 API.add_resource(UpdateInterventionComment, '/interventions/<int:intervention_id>/comment')
+API.add_resource(Redflag, '/redflags/<int:redflag_id>')
+API.add_resource(Redflags, '/redflags')
+API.add_resource(UpdateRedflagStatus, '/redflags/<int:redflag_id>/status')
+API.add_resource(UpdateRedflagLocation, '/redflags/<int:redflag_id>/location')
+API.add_resource(UpdateRedflagComment, '/redflags/<int:redflag_id>/comment')
+
