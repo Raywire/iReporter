1bfeea3a66ae3d8bd9c7903f0a96f7b455a76ef2
Ryan Wire <ryanwire@outlook.com>
2019-01-14 17:13:07 +0300

    [163190715] Adds more tests

--- UI/js/tests/ireporterTest.js
+++ UI/js/tests/ireporterTest.js
@@ -1,8 +1,23 @@
-describe('Testing the functionality, convert utc to local time', ()=>{
-    it('should convert utc to Kenyan Time', ()=>{
-        let utcTime = "Sun, 13 Jan 2019 20:36:27 GMT"
+describe('Testing the functionality, convert utc to local time', () => {
+  it('should convert utc to Kenyan Time', () => {
+    let utcTime = "Sun, 13 Jan 2019 20:36:27 GMT"
 
-      expect(convertToLocalTime(utcTime)).toBe("Sun, Jan 13th 2019, 11:36:27 pm +03:00");
-    })
+    expect(convertToLocalTime(utcTime)).toBe("Sun, Jan 13th 2019, 11:36:27 pm +03:00");
   })
-  
+})
+
+describe('Testing the functionality, to get a cookie', () => {
+  it('should check if a cookie exists', () => {
+    document.cookie = "username=John Doe";
+
+    expect(getCookie("username")).toBe("John Doe");
+  })
+})
+
+describe('Testing the functionality, to check if cookies are reset on logout', () => {
+  it('should check if cookies are cleared on logout', () => {
+    document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+
+    expect(getCookie("username")).toBe("");
+  })
+})
--- UI/js/tests/tests_config.js
+++ UI/js/tests/tests_config.js
@@ -1,7 +1,3 @@
-const config = {
-    root : 'https://pure-wildwood-82378.herokuapp.com/api/v2/'
-  }
-
 const user = {
     token : 'token', name : 'John', email : 'john@doe.com', username : 'johndoe', isAdmin : true, isLoggedIn : 'False'
   };
