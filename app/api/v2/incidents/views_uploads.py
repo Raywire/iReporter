eedc6d19566fbcb70381b528d88875d0c55d696b
Ryan Wire <ryanwire@outlook.com>
2019-02-07 10:33:24 +0300

    Fix upload failure

--- app/api/v2/incidents/views_uploads.py
+++ app/api/v2/incidents/views_uploads.py
@@ -16,28 +16,28 @@ class UploadInterventionImage(Resource):
     @token_required
     def patch(current_user, self, intervention_id):
         """method to upload image to an intervention"""
-        upload_image = self.intervention_upload_model.upload_incident_file(
+        upload_status = self.intervention_upload_model.upload_incident_file(
             "intervention", intervention_id, current_user['id'], 'images')
 
-        if upload_image is False:
+        if upload_status is False:
             return jsonify({
                 "status": 401,
                 "message": "You cannot upload a photo for this incident"
             })
 
-        if upload_image is None:
+        if upload_status is None:
             return jsonify({
                 "status": 404,
                 "message": "Intervention does not exist"
             })
 
-        if upload_image == 'No uploadFile name in form' or upload_image == 'File type not supported':
+        if upload_status == 'No uploadFile name in form' or upload_status == 'File type not supported':
             return jsonify({
                 "status": 400,
                 "message": upload_status
             })
 
-        if upload_image is True:
+        if upload_status is True:
             return jsonify({
                 "status": 200,
                 "data": [{
@@ -53,28 +53,28 @@ class UploadInterventionVideo(Resource):
     @token_required
     def patch(current_user, self, intervention_id):
         """method to upload videos to an intervention"""
-        upload_video = IncidentModel().upload_incident_file(
+        upload_status = IncidentModel().upload_incident_file(
             "intervention", intervention_id, current_user['id'], 'videos')
 
-        if upload_video is None:
+        if upload_status is None:
             return jsonify({
                 "status": 404,
                 "message": "Intervention does not exist"
             })
 
-        if upload_video is False:
+        if upload_status is False:
             return jsonify({
                 "status": 401,
                 "message": "You cannot upload a video for this incident"
             })
 
-        if upload_video == 'File type not supported' or upload_video == 'No uploadFile name in form':
+        if upload_status == 'File type not supported' or upload_status == 'No uploadFile name in form':
             return jsonify({
                 "status": 400,
-                "message": upload_video
+                "message": upload_status
             })
 
-        if upload_video is True:
+        if upload_status is True:
             return jsonify({
                 "status": 200,
                 "data": [{
@@ -102,7 +102,7 @@ class UploadRedflagImage(Resource):
                 "message": "You cannot upload a photo for this incident"
             })
 
-        if upload_status == 'File type not supported' or upload_status == 'No uploadFile name in form':
+        if upload_status == 'No uploadFile name in form' or upload_status == 'File type not supported':
             return jsonify({
                 "status": 400,
                 "message": upload_status
@@ -174,13 +174,11 @@ class Video(Resource):
 
 class Image(Resource):
     """"Contains method to get an image"""
-    def __init__(self):
-        self.get_image = IncidentModel()
 
     @token_required
     def get(current_user, self, filename):
         """Method to get an image"""
-        image_url = self.get_image.get_file_url('images', filename)
+        image_url = IncidentModel().get_file_url('images', filename)
         if image_url is None:
             return {
                 "status": 404,
